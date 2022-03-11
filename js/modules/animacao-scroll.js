ecf2f5d4ecc57f0210bb0e0103589abcc6c03118
Gusta-SX <gugaa24@gmail.com>
2022-03-11 16:24:28 -0300

    implement modules

---
+++ js/modules/animacao-scroll.js
@@ -0,0 +1,18 @@
+export default function initAnimationScroll() {
+  const sections = document.querySelectorAll('[data-anime="scroll"]');
+  const windowUser = window.innerHeight * 0.5;
+
+  function animaScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = sectionTop - windowUser < 0;
+      if (isSectionVisible) {
+        section.classList.add("ativo");
+      } else {
+        section.classList.remove("ativo");
+      }
+    });
+  }
+  animaScroll();
+  window.addEventListener("scroll", animaScroll);
+}

1456eeacf1ecc1ceaf536b59c011f796c42eddbc
Gusta-SX <gugaa24@gmail.com>
2022-03-14 11:57:01 -0300

    implement tooltip

--- js/script.js
+++ js/script.js
@@ -2,10 +2,13 @@ import initScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/animacao-scroll.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
 
 console.log(this)
-
+initModal();
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
+initTooltip
